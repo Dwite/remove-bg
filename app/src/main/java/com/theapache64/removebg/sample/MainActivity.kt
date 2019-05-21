1e4154e15997dfc4fe0418f0ecc215335af8ad54
theapache64 <theapache64@gmail.com>
2019-05-21 21:59:27 +0530

    🌄 It worked (backup)

--- app/src/main/java/com/theapache64/removebg/sample/MainActivity.kt
+++ app/src/main/java/com/theapache64/removebg/sample/MainActivity.kt
@@ -2,7 +2,10 @@ package com.theapache64.removebg.sample
 
 import android.Manifest
 import android.content.Intent
+import android.graphics.Bitmap
+import android.graphics.drawable.Drawable
 import android.os.Bundle
+import android.os.Environment
 import android.widget.ImageView
 import android.widget.Toast
 import androidx.annotation.StringRes
@@ -12,20 +15,33 @@ import butterknife.BindView
 import butterknife.ButterKnife
 import butterknife.OnClick
 import com.bumptech.glide.Glide
+import com.bumptech.glide.request.target.CustomTarget
+import com.bumptech.glide.request.transition.Transition
 import com.esafirm.imagepicker.features.ImagePicker
 import com.esafirm.imagepicker.model.Image
 import com.karumi.dexter.Dexter
-import com.karumi.dexter.listener.multi.DialogOnAnyDeniedMultiplePermissionsListener
+import com.karumi.dexter.listener.PermissionDeniedResponse
+import com.karumi.dexter.listener.PermissionGrantedResponse
+import com.karumi.dexter.listener.single.BasePermissionListener
+import com.karumi.dexter.listener.single.CompositePermissionListener
+import com.karumi.dexter.listener.single.DialogOnDeniedPermissionListener
 import com.theapache64.twinkill.logger.info
 import java.io.File
 
 
 class MainActivity : AppCompatActivity() {
 
+    private val projectDir by lazy {
+        val rootPath = Environment.getExternalStorageDirectory().absolutePath
+        File("$rootPath/remove-bg")
+    }
     private var image: Image? = null
 
     @BindView(R.id.iv_input)
     lateinit var ivInput: ImageView
+
+    @BindView(R.id.iv_output)
+    lateinit var ivOutput: ImageView
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -49,19 +65,103 @@ class MainActivity : AppCompatActivity() {
 
     @OnClick(R.id.b_process)
     fun onProcessClicked() {
-        // Check permission
-        checkPermission()
+        if (image != null) {
+
+            info("Image is ${image!!.path}")
+
+            // Check permission
+            checkPermission {
+
+                info("Permission granted")
+
+                // permission granted, compress the image now
+                compressImage { bitmap ->
+
+                    info("Image compressed")
+
+                    saveImage(bitmap) { file ->
+
+                        info("Compressed image saved to ${file.absolutePath}, and removing bg...")
+
+                        // image saved, now upload
+                        removeBgFromImage(file) { output ->
+
+                            info("background removed from bg , and output is $output")
+                            runOnUiThread {
+                                ivOutput.setImageBitmap(output)
+                            }
+                        }
+                    }
+                }
+            }
+
+        } else {
+            toast(R.string.error_no_image_selected)
+        }
     }
 
-    fun checkPermission() {
+    private fun removeBgFromImage(file: File, onRemoved: (output: Bitmap) -> Unit) {
+        // Do Api Call here
+        RemoveBg.from(file, onRemoved)
+    }
 
-        val dialogListener = DialogOnAnyDeniedMultiplePermissionsListener.Builder.withContext(this)
+    private fun saveImage(bitmap: Bitmap, onSaved: (file: File) -> Unit) {
+
+        // Create project dir
+        if (!projectDir.exists()) {
+            projectDir.mkdir()
+        }
+
+        // Create image file
+        val imageFile = File("$projectDir/${System.currentTimeMillis()}.jpg")
+        imageFile.outputStream().use { out ->
+            bitmap.compress(Bitmap.CompressFormat.JPEG, 100, out)
+            out.flush()
+        }
+
+        onSaved(imageFile)
+    }
+
+    private fun compressImage(onLoaded: (bitmap: Bitmap) -> Unit) {
+
+        Glide.with(this)
+            .asBitmap()
+            .load(File(image!!.path))
+            .into(object : CustomTarget<Bitmap>() {
+                override fun onLoadCleared(placeholder: Drawable?) {
+
+                }
+
+                override fun onResourceReady(resource: Bitmap, transition: Transition<in Bitmap>?) {
+                    onLoaded(resource)
+                }
+            })
+    }
+
+    private fun checkPermission(onPermissionChecked: () -> Unit) {
+
+        val deniedListener = DialogOnDeniedPermissionListener.Builder.withContext(this)
             .withTitle(R.string.title_permission)
+            .withMessage(R.string.message_permission)
+            .withButtonText(R.string.action_ok)
+            .build()
+
+        val permissionListener = object : BasePermissionListener() {
+            override fun onPermissionGranted(response: PermissionGrantedResponse?) {
+                onPermissionChecked()
+            }
+
+            override fun onPermissionDenied(response: PermissionDeniedResponse?) {
+                toast(R.string.error_permission)
+            }
+        }
+
+        val listener = CompositePermissionListener(permissionListener, deniedListener)
 
         Dexter.withActivity(this)
             .withPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE)
-
-
+            .withListener(listener)
+            .check()
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
--- library/src/main/java/com/theapache64/removebg/sample/RemoveBg.kt
+++ library/src/main/java/com/theapache64/removebg/sample/RemoveBg.kt
@@ -1,11 +1,65 @@
 package com.theapache64.removebg.sample
 
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.util.Log
+import com.theapache64.twinkill.logger.info
+import okhttp3.*
+import java.io.File
+import java.io.IOException
+
 object RemoveBg {
 
-    private lateinit var apiKey: String
+    // Strings
+    private const val API_ENDPOINT = "https://api.remove.bg/v1.0/removebg"
+
+    private var apiKey: String? = null
+
+    private val okHttpClient by lazy {
+        OkHttpClient.Builder()
+            .build()
+    }
 
     fun init(apiKey: String) {
         this.apiKey = apiKey
+    }
+
+    fun from(file: File, onRemoved: (bitmap: Bitmap) -> Unit) {
+
+        require(apiKey != null) { "You must call RemoveBg.init before calling RemoveBg.from" }
+
+        // file
+        val filePart = MultipartBody.create(MediaType.parse("image/png"), file)
+        val body = MultipartBody.Builder()
+            .setType(MultipartBody.FORM)
+            .addFormDataPart("size", "auto")
+            .addFormDataPart("image_file", "image_file", filePart)
+            .build()
+
+
+        // new request
+        val request = Request.Builder()
+            .url(API_ENDPOINT)
+            .addHeader("X-Api-Key", apiKey!!)
+            .post(body)
+            .build()
+
+        okHttpClient.newCall(request).enqueue(object : Callback {
+            override fun onFailure(call: Call, e: IOException) {
+                e.printStackTrace()
+            }
+
+            override fun onResponse(call: Call, response: Response) {
+
+                if (response.isSuccessful) {
+                    response.body()!!.byteStream().let { bytesStream ->
+                        val bmp = BitmapFactory.decodeStream(bytesStream)
+                        onRemoved(bmp)
+                    }
+                }
+            }
+        })
+
     }
 
 }
